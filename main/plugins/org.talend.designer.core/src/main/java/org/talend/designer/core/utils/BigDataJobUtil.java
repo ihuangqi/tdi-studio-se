cb27fc99d0290f81f980b8ff354da71e8d66f575
sponomarova <42870019+sponomarova@users.noreply.github.com>
2023-06-01 08:26:10 +0300

    feat(TBD-15036): Beta - Universal Spark 3.1 for HDInsight 5.0 (#8791)

--- main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/utils/BigDataJobUtil.java
+++ main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/utils/BigDataJobUtil.java
@@ -66,8 +66,9 @@ public class BigDataJobUtil {
                 || isBDJobWithFramework(ERepositoryObjectType.PROCESS_STORM, HadoopConstants.FRAMEWORK_SPARKSTREAMING)) {
             List<? extends IElementParameter> parameters = process.getElementParametersWithChildrens();
             for (IElementParameter pt : parameters) {
-                if (pt.getName().equals("DISTRIBUTION") //$NON-NLS-1$
-                        && EHadoopDistributions.MICROSOFT_HD_INSIGHT.getName().equals(pt.getValue())) {
+                boolean isHDISparkMode = HadoopConstants.SPARK_MODE.equals(pt.getName()) && HadoopConstants.SPARK_MODE_HDI.equals(pt.getValue());
+                boolean isHDIDistribution = pt.getName().equals("DISTRIBUTION") && EHadoopDistributions.MICROSOFT_HD_INSIGHT.getName().equals(pt.getValue());
+                if (isHDISparkMode || isHDIDistribution) {
                     isSparkWithHDInsight = true;
                 }
             }
@@ -98,11 +99,10 @@ public class BigDataJobUtil {
             if (isBDJobWithFramework(ERepositoryObjectType.PROCESS_MR, HadoopConstants.FRAMEWORK_MAPREDUCE)) {
                 List<? extends IElementParameter> parameters = process.getElementParametersWithChildrens();
                 for (IElementParameter pt : parameters) {
-                    if (pt.getName().equals("DISTRIBUTION") //$NON-NLS-1$
-                            && EHadoopDistributions.MICROSOFT_HD_INSIGHT.getName().equals(pt.getValue())) {
-                        isMRWithHDInsight = true;
-                        break;
-                    }
+             boolean isHDIDistribution = pt.getName().equals("DISTRIBUTION") && EHadoopDistributions.MICROSOFT_HD_INSIGHT.getName().equals(pt.getValue());
+                if (isHDIDistribution) {
+                    isMRWithHDInsight = true;
+                   }
                 }
             }
         }
