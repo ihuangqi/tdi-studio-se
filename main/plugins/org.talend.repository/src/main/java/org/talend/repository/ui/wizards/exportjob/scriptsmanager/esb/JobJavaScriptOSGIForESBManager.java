a4471dbfa78ffa3ac60cb854dab1c061e3f187a9
apoltavtsev <apoltavtsev@gmail.com>
2023-06-07 10:16:03 +0200

    TDI-49653: Avoid using javax.ws.rs dependencies within OSGi bundle (#8809)

--- main/plugins/org.talend.repository/src/main/java/org/talend/repository/ui/wizards/exportjob/scriptsmanager/esb/JobJavaScriptOSGIForESBManager.java
+++ main/plugins/org.talend.repository/src/main/java/org/talend/repository/ui/wizards/exportjob/scriptsmanager/esb/JobJavaScriptOSGIForESBManager.java
@@ -1141,6 +1141,8 @@ public class JobJavaScriptOSGIForESBManager extends JobJavaScriptsManager {
 
         Set<String> fileterdImportPackage = new HashSet<>();
 
+        privateNonRepetitivePackages.removeIf(p-> p.startsWith("javax.ws.rs"));
+        
         for (String p : importNonRepetitivePackages) {
             if (!privateNonRepetitivePackages.contains(p.replace(RESOLUTION_OPTIONAL, "")) || p.startsWith("routines")) {
                 fileterdImportPackage.add(p);
