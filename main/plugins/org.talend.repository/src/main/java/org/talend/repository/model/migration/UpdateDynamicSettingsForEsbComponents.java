a4b963cea5d06791ae494bbaed3a7e28a67d319a
Jill Yan <yan95599@gmail.com>
2023-08-30 09:27:49 +0800

    Jill/m80/appint-35837 (#9055)
    * APPINT-35837 Correct dynamic settings
    
    * APPINT-35837 Correct dynamic settings

---
+++ main/plugins/org.talend.repository/src/main/java/org/talend/repository/model/migration/UpdateDynamicSettingsForEsbComponents.java
@@ -0,0 +1,65 @@
+package org.talend.repository.model.migration;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import org.talend.commons.exception.ExceptionHandler;
+import org.talend.commons.exception.PersistenceException;
+import org.talend.core.model.components.ComponentUtilities;
+import org.talend.core.model.components.ModifyComponentsAction;
+import org.talend.core.model.components.conversions.IComponentConversion;
+import org.talend.core.model.components.filters.IComponentFilter;
+import org.talend.core.model.components.filters.NameComponentFilter;
+import org.talend.core.model.migration.AbstractJobMigrationTask;
+import org.talend.core.model.properties.Item;
+import org.talend.designer.core.model.utils.emf.talendfile.ElementParameterType;
+import org.talend.designer.core.model.utils.emf.talendfile.NodeType;
+import org.talend.designer.core.model.utils.emf.talendfile.ProcessType;
+
+public class UpdateDynamicSettingsForEsbComponents extends AbstractJobMigrationTask {
+
+    @Override
+    public Date getOrder() {
+        GregorianCalendar gc = new GregorianCalendar(2023, 8, 28, 12, 10, 0);
+        return gc.getTime();
+    }
+
+    @Override
+    public ExecutionResult execute(Item item) {
+        final ProcessType processType = getProcessType(item);
+        String[] compNames = { "tRESTClient", "tESBConsumer"}; //$NON-NLS-1$
+
+        IComponentConversion action = new IComponentConversion() {
+
+            public void transform(NodeType node) {
+                if (node == null) {
+                    return;
+                }
+
+                ElementParameterType property = ComponentUtilities.getNodeProperty(node, "AUTH_TYPE");//$NON-NLS-1$
+                if (property != null) {
+                    property.unsetContextMode();
+                    String value = ComponentUtilities.getNodePropertyValue(node, "AUTH_TYPE"); //$NON-NLS-1$
+                    String newValue = value.replace("\"", "");
+                    ComponentUtilities.setNodeValue(node, "AUTH_TYPE", newValue);//$NON-NLS-1$ //$NON-NLS-2$
+                }
+            }
+
+        };
+
+        for (String name : compNames) {
+            IComponentFilter filter = new NameComponentFilter(name);
+
+            try {
+                ModifyComponentsAction.searchAndModify(item, processType, filter, Arrays.<IComponentConversion> asList(action));
+            } catch (PersistenceException e) {
+                ExceptionHandler.process(e);
+                return ExecutionResult.FAILURE;
+            }
+        }
+
+        return ExecutionResult.SUCCESS_NO_ALERT;
+    }
+
+}
